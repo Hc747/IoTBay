415c470e13a8580b2096c8d344642382e2dd69ee
Hc747 <harrisoncole05@gmail.com>
2020-05-23 15:02:34 +1000

    - Add SQLFunction interface (Function with SQLException in apply method signature). - Add withConnection and withStatement methods into ConnectionProvider to handle resource management.

--- src/main/java/au/edu/uts/isd/iotbay/database/ConnectionProvider.java
+++ src/main/java/au/edu/uts/isd/iotbay/database/ConnectionProvider.java
@@ -2,9 +2,31 @@ package au.edu.uts.isd.iotbay.database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public interface ConnectionProvider extends AutoCloseable {
 
+    /**
+     * Not recommended for direct use, as it requires direct resource management.
+     */
     Connection connection() throws SQLException;
 
+    default <T> T withConnection(SQLFunction<Connection, T> action) throws SQLException {
+        try (Connection connection = connection()) {
+            return action.apply(connection);
+        }
+    }
+
+    default <T> T withStatement(SQLFunction<Statement, T> action) throws SQLException {
+        try (Connection connection = connection()) {
+            try (Statement statement = connection.createStatement()) {
+                return action.apply(statement);
+            }
+        }
+    }
+
+    @FunctionalInterface
+    interface SQLFunction<T, R> {
+        R apply(T t) throws SQLException;
+    }
 }
