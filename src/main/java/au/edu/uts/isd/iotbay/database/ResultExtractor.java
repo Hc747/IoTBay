4cae399ff23500930f01f431460d24a6cd39e731
Hc747 <harrisoncole05@gmail.com>
2020-05-23 21:39:44 +1000

    - introduce ResultExtractor interface for Mapping Complex types from ResultSet

---
+++ src/main/java/au/edu/uts/isd/iotbay/database/ResultExtractor.java
@@ -0,0 +1,32 @@
+package au.edu.uts.isd.iotbay.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@FunctionalInterface
+public interface ResultExtractor<T> {
+
+    T extract(ResultSet results) throws SQLException;
+
+    default T single(ResultSet results) throws SQLException {
+        try (ResultSet rs = results) {
+            if (rs.next()) {
+                return extract(rs);
+            }
+            return null;
+        }
+    }
+
+    default Collection<T> all(ResultSet results) throws SQLException {
+        try (ResultSet rs = results) {
+            final List<T> elements = new ArrayList<>(results.getFetchSize());
+            while (rs.next()) {
+                elements.add(extract(rs));
+            }
+            return elements;
+        }
+    }
+}
