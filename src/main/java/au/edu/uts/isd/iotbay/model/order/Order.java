90e001588dc8dc77bbcdf103d303d592d2676b8f
jackGiant24 <felix240600@gmail.com>
2020-05-30 01:30:45 +1000

    order repo

--- src/main/java/au/edu/uts/isd/iotbay/model/order/Order.java
+++ src/main/java/au/edu/uts/isd/iotbay/model/order/Order.java
@@ -1,4 +1,13 @@
 package au.edu.uts.isd.iotbay.model.order;
 
+import java.util.*;
+
 public class Order {
+    private int id, invoiceId, addressId, paymentMethodId, productId;
+    public enum status
+    {
+        SAVED,
+        SUBMITTED,
+        CANCELLED;
+    }
 }
