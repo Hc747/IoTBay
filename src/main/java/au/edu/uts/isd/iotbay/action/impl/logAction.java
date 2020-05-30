5d958220e963bea1d35c9ab682793a60ffd77e68
Corey East <corey.e.east@gmail.com>
2020-05-30 16:24:00 +1000

    - add log action - add user action

---
+++ src/main/java/au/edu/uts/isd/iotbay/action/impl/logAction.java
@@ -0,0 +1,8 @@
+package au.edu.uts.isd.iotbay.action.impl;
+
+import au.edu.uts.isd.iotbay.action.ActionRegistry;
+import au.edu.uts.isd.iotbay.action.AuthenticatedAction;
+import sun.text.normalizer.ICUBinary;
+
+public class logAction extends AuthenticatedAction {
+}
