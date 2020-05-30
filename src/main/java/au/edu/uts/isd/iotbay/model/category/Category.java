8bead1823566ee49c9331e487a194428ce371e3c
Matt Lockie <matt.lockie101@gmail.com>
2020-05-30 11:27:56 +1000

    Add Category Class

---
+++ src/main/java/au/edu/uts/isd/iotbay/model/category/Category.java
@@ -0,0 +1,11 @@
+package au.edu.uts.isd.iotbay.model.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class Category {
+    private Integer id;
+    private String name;
+    private String description;
+    private Boolean enabled;
+}
