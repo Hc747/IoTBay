182af4f8fc6935dfe422001a16f2262e110e815c
Frank-326 <jiangjiahao0326@gmail.com>
2020-05-30 15:14:41 +1000

    updated address and shipment repository

--- src/main/java/au/edu/uts/isd/iotbay/repository/address/InMemoryAddressRepository.java
+++ src/main/java/au/edu/uts/isd/iotbay/repository/address/InMemoryAddressRepository.java
@@ -2,27 +2,53 @@ package au.edu.uts.isd.iotbay.repository.address;
 
 import au.edu.uts.isd.iotbay.model.address.Address;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class InMemoryAddressRepository implements AddressRepository {
+    private static final AtomicInteger SEQUENCE = new AtomicInteger(1);
+
+    private final Map<Integer, Address> address;
+
+    private InMemoryAddressRepository(Map<Integer, Address> address) {
+        this.address = Objects.requireNonNull(address);
+    }
 
     @Override
     public Collection<Address> all() {
-        return null;
+        return new ArrayList<>(address.values());
     }
 
     @Override
     public Address create(Address instance) {
-        return null;
+        final Integer id = SEQUENCE.getAndIncrement();
+        return address.compute(id, (k, v) -> {
+            instance.setId(id);
+            return instance;
+        });
     }
 
     @Override
     public Address update(Address instance) {
-        return null;
+        address.put(instance.getId(), instance);
+        return instance;
     }
 
     @Override
     public Address delete(Address instance) {
-        return null;
+        return address.remove(instance.getId(), instance) ? instance : null;
+    }
+
+    public static InMemoryAddressRepository concurrent() {
+        return new InMemoryAddressRepository(new ConcurrentHashMap<>());
+    }
+
+    public static InMemoryAddressRepository synchronised() {
+        return new InMemoryAddressRepository(Collections.synchronizedMap(new HashMap<>()));
+    }
+
+    public static InMemoryAddressRepository unsynchronised() {
+        return new InMemoryAddressRepository(new HashMap<>());
     }
 }
