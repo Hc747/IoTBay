67724c0c9e61ad11090992323296a54501c23bb9
jackGiant24 <felix240600@gmail.com>
2020-05-30 11:23:11 +1000

    Implement Order Model

--- src/main/java/au/edu/uts/isd/iotbay/repository/Order/InMemoryOrderRepository.java
+++ src/main/java/au/edu/uts/isd/iotbay/repository/Order/InMemoryOrderRepository.java
@@ -1,5 +1,12 @@
 package au.edu.uts.isd.iotbay.repository.Order;
 
+import au.edu.uts.isd.iotbay.model.order.Order;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class InMemoryOrderRepository {
+
 
 }
--- src/main/java/au/edu/uts/isd/iotbay/repository/Order/OrderRepository.java
+++ src/main/java/au/edu/uts/isd/iotbay/repository/Order/OrderRepository.java
@@ -3,17 +3,18 @@ package au.edu.uts.isd.iotbay.repository.Order;
 import au.edu.uts.isd.iotbay.repository.Repository;
 import au.edu.uts.isd.iotbay.model.order.Order;
 import au.edu.uts.isd.iotbay.database.ConnectionProvider;
+
 import java.util.Optional;
 
 public interface OrderRepository extends Repository<Order>{
     Optional<Order> findByUsername(String username);
 
-    static OrderRepository create (ConnectionProvider datasource) {
-        if(datasource == null)
-        {
-            //Implement InMemoryOrderRepository
+    static OrderRepository create(ConnectionProvider datasource) {
+        if (datasource == null) {
+            //return in memory implementation
+            return null;
         }
-        //Implement PersistentOrderRepository
+        //return persistent implementation
         return null;
     }
 }
