534e8fe700248e1e6dd42d8f025eecd22699a199
Hc747 <harrisoncole05@gmail.com>
2020-06-01 23:24:47 +1000

    - delete InvoiceGuest and InvoiceUser classes.

--- src/main/java/au/edu/uts/isd/iotbay/model/invoice/InvoiceGuest.java
+++
@@ -1,9 +0,0 @@
-package au.edu.uts.isd.iotbay.model.invoice;
-
-import lombok.*;
-
-@Data
-public class InvoiceGuest extends Invoice {
-    String firstName, lastName, email;
-
-}
--- src/main/java/au/edu/uts/isd/iotbay/model/invoice/InvoiceUser.java
+++
@@ -1,9 +0,0 @@
-package au.edu.uts.isd.iotbay.model.invoice;
-
-import lombok.*;
-
-@Data
-@AllArgsConstructor
-public class InvoiceUser extends Invoice{
-    protected int userID;
-}
