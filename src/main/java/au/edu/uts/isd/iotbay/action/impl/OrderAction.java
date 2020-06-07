ad01ea46cdd2de55fc7dedda5e8f3975e832c55d
jackGiant24 <felix240600@gmail.com>
2020-06-07 15:03:43 +1000

    Order Action Update WIP

--- src/main/java/au/edu/uts/isd/iotbay/action/impl/OrderAction.java
+++ src/main/java/au/edu/uts/isd/iotbay/action/impl/OrderAction.java
@@ -6,8 +6,14 @@ import au.edu.uts.isd.iotbay.action.UnauthenticatedAction;
 import au.edu.uts.isd.iotbay.model.order.Order;
 import au.edu.uts.isd.iotbay.model.order.OrderProduct;
 import au.edu.uts.isd.iotbay.model.order.OrderStatus;
+import au.edu.uts.isd.iotbay.model.payment.PaymentMethod;
+import au.edu.uts.isd.iotbay.model.product.Product;
+import au.edu.uts.isd.iotbay.model.shipment.Shipment;
 import au.edu.uts.isd.iotbay.model.user.User;
 import au.edu.uts.isd.iotbay.repository.order.OrderRepository;
+import au.edu.uts.isd.iotbay.repository.payment.PaymentMethodRepository;
+import au.edu.uts.isd.iotbay.repository.product.ProductRepository;
+import au.edu.uts.isd.iotbay.repository.shipment.ShipmentRepository;
 import au.edu.uts.isd.iotbay.util.AuthenticationUtil;
 import lombok.SneakyThrows;
 import org.bson.types.ObjectId;
@@ -17,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.text.DecimalFormat;
+import java.time.LocalDate;
 
 import static au.edu.uts.isd.iotbay.util.Validator.isNullOrEmpty;
 
@@ -57,4 +64,32 @@ public class OrderAction extends Action {
         repository.delete(order);
         message = "Order was successfully  deleted";
     }
+
+    @SneakyThrows
+    private void update(IoTBayApplicationContext ctx, HttpSession session, HttpServletRequest request) {
+        final String identifier = request.getParameter("id");
+
+        if (isNullOrEmpty(identifier)) {
+            reject("No Order Id found");
+        }
+
+        if (!ObjectId.isValid(identifier)) {
+            reject("Order Id was not valid.");
+        }
+
+        final OrderRepository repository = ctx.getOrders();
+        final Order order = repository.findById(identifier);
+
+    }
+    @SneakyThrows
+    private void create(IoTBayApplicationContext ctx, HttpSession session, HttpServletRequest request){
+        /*final User user = authenticate(session);
+        LocalDate date = LocalDate.now();
+        final ShipmentRepository shipmentRepository= ctx.getShipments();
+        Shipment shipment = shipmentRepository.findById(ObjectId);
+        final PaymentMethodRepository paymentMethodRepository = ctx.getPayments();
+        PaymentMethod paymentMethod = paymentMethodRepository.findById(ObjectId);*/
+
+    }
+
 }
